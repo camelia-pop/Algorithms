ea76c396c345bc830f4d95bf2d21169571407149
camelia-pop <34198555+camelia-pop@users.noreply.github.com>
2017-12-02 18:19:39 -0600

    Update Percolation_test.cpp

--- 1. Union-find/Percolation_test.cpp
+++ 1. Union-find/Percolation_test.cpp
@@ -1,3 +1,12 @@
+/*
+This programming assignment estimates the value of the critical probability p_c when a system begins to percolate.
+The system is here a n-by-n matrix of closed sites.
+We choose with equal probability to open a site. We stop the first time when there is a path of open sites connecting the first row 
+of the matrix to the last row. The proportion of open sites is a sample of the critical probability when the system begins to percolate.
+A brief introduction to percolation theory and the definition of critical probability can be found at the Wikipedia link:
+https://en.wikipedia.org/wiki/Percolation_theory
+*/
+
 #include "Percolation.h"
 #include "PercolationStats.h"
 #include <iostream>
