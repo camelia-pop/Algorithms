0b90d91dcd65b3d0e75578bddf0f075a46930bec
camelia-pop <34198555+camelia-pop@users.noreply.github.com>
2017-12-07 10:16:52 -0600

    Update Forward_list.cpp

--- 5. Mergesort/Forward_list.cpp
+++ 5. Mergesort/Forward_list.cpp
@@ -42,7 +42,10 @@ https://en.wikipedia.org/wiki/Gilbert%E2%80%93Shannon%E2%80%93Reeds_model
 
 See the first of the equivalent descriptions of the model. 
 
-We divide the list into two sublists and we assume that each are independetly perfectly shuffled. One list has length l and the other has length k. Then a ordering of the elements appear with probability 1/l! and 1/k! in each list. We merge the two lists by choosing an element from the each list with probability proportional to the number of elements remaining in the corresponding list. This procedure is implemented in the merge function.
+We divide the list into two sublists and we assume that each are independetly perfectly shuffled. 
+One list has length l and the other has length k. Then a ordering of the elements appear with probability 1/l! and 1/k! in each list.
+We merge the two lists by choosing an element from each list with probability proportional to the number of elements remaining in the 
+corresponding list. This procedure is implemented in the merge function.
 */
 void Forward_list::shuffle() {
 	srand((unsigned) time(0));
@@ -62,8 +65,7 @@ void Forward_list::divide_and_merge
 	
 	divide_and_merge(beforeItem, item, n/2);	
 	divide_and_merge(beforeMid, mid, n - n/2);
-	merge(item, n/2, beforeMid, mid, n - n/2);
-	
+	merge(item, n/2, beforeMid, mid, n - n/2);	
 }
 
 // find the middle element between item and the (n-1)th element after item
@@ -88,9 +90,13 @@ pair<Node*, Node*> Forward_list::middle
 }
 
 /* 
-Assume that the sublist that starts from left and contains l elements is shuffled. 
-Also assume that the sublist that starts from right and contains k elements is shuffled.
-The following procedure merges the two suffled lists into one suffled list of length l + k using the rule: the next element to be added to the merged list is chosen with probability p from the first list and with probability 1-p from the second list, where p is the number of elements that were not already chosen from the first list divided by the total number of elements that were not chosen from the two lists.
+Assume that the sublist that starts from left contains l elements and is shuffled. 
+Also assume that the sublist that starts from right contains k elements and is shuffled.
+The following procedure merges the two suffled lists into one suffled list of length l + k using the rule: 
+the next element to be added to the merged list is chosen with probability p from the first list 
+and with probability 1-p from the second list, 
+where p is the number of elements that were not already chosen from the first list 
+divided by the total number of elements that were not chosen from the two lists.
 */
 void Forward_list::merge(Node *left, size_t l, 
 	Node *beforeRight, Node *right, size_t k) {
